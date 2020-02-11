b7451a2b081af290df0bb5dc648540f4bbb87e17
huskystuart <hc01843@srcs.org>
2020-02-11 14:37:35 -0800

    Watch Where You're Going 1:00

---
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 9.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
